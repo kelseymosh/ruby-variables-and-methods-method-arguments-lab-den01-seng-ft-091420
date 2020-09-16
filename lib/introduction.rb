75add3626297c43038d399ddd5f9f0b58ae6976a
Kelsey Oshiro <kelseymosh@gmail.com>
2020-09-16 16:17:10 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  name = "Bob"
+    puts "Hi, my name is #{name}."
+  end
+
+
+def introduction_with_language(name, language)
+   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
